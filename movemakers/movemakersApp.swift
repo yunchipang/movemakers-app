eeaa0141a8858dea460a9721076541dc21796764
yunchipang <yunchipang@gmail.com>
2024-05-08 10:23:53 -0700

    Initial Commit

---
+++ movemakers/movemakersApp.swift
@@ -0,0 +1,17 @@
+//
+//  movemakersApp.swift
+//  movemakers
+//
+//  Created by Yunchi Pang on 5/8/24.
+//
+
+import SwiftUI
+
+@main
+struct movemakersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
