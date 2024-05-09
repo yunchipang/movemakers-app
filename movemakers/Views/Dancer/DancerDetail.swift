28b677b5ec5b50fdbbf82f679dead2205159ba3f
yunchipang <yunchipang@gmail.com>
2024-05-09 09:01:33 -0700

    create fav button view

--- movemakers/Views/Dancer/DancerDetail.swift
+++ movemakers/Views/Dancer/DancerDetail.swift
@@ -8,17 +8,29 @@
 import SwiftUI
 
 struct DancerDetail: View {
+    @Environment(ModelData.self) var modelData
     var dancer: Dancer
     
+    var dancerIndex: Int {
+        modelData.dancers.firstIndex(where: { $0.id == dancer.id })!
+    }
+    
     var body: some View {
+        @Bindable var modelData = modelData
+        
         ScrollView {
+            
             CircleImage(image: dancer.profilePic)
                 .padding(.top, 130)
                 .offset(y: -130)
                 .padding(.bottom, -130)
+            
             VStack(alignment: .leading) {
-                Text(dancer.name)
-                    .font(.title)
+                HStack {
+                    Text(dancer.name)
+                        .font(.title)
+                    FavoriteButton(isSet: $modelData.dancers[dancerIndex].isFavorite)
+                }
                 
                 Divider()
                 
@@ -33,5 +45,7 @@ struct DancerDetail: View {
 }
 
 #Preview {
-    DancerDetail(dancer: ModelData().dancers[0])
+    let modelData = ModelData()
+    return DancerDetail(dancer: modelData.dancers[0])
+        .environment(modelData)
 }
---
+++ movemakers/Views/Helper/FavoriteButton.swift
@@ -0,0 +1,28 @@
+//
+//  FavoriteButton.swift
+//  movemakers
+//
+//  Created by Yunchi Pang on 5/9/24.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+
+
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundStyle(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+
+#Preview {
+    FavoriteButton(isSet: .constant(true))
+}
